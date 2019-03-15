71360d6595cdbd3d6ddc4456c3f9d24762329292
seyidaniels <seyidaniels@gmail.com>
2019-03-15 05:59:13 +0100

    Rgister Bug

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -40,13 +40,13 @@ class RegisterController extends Controller
 
         $data['email_token'] = str_random(30); //Generate verification code
 
-        DB::transaction(function () use ($data, $request) {
+        DB::transaction(function () use ($data) {
 
             $user = User::create($data);
 
             $data['message'] = 'Thank you for creating an account to participate in the Professor Ogundipe Innovative Challenge. To continue please confirm your email by clicking the button below';
 
-            $request->has('is_admin') ? $this->registerAdmin($data, $user) : $user->notify(new WelcomeEmail($data));
+            in_array('is_admin', $data) ? $this->registerAdmin($data, $user) : $user->notify(new WelcomeEmail($data));
         }, 4);
 
         return response()->json(['success' => true, 'message' => 'Thanks for signing up! Please check your email to complete your registration.'], 200);

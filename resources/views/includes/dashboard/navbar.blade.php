52db446dba8b4592623425d258a438360de3630e
StaceyCreates <juliehiva@gmail.com>
2020-02-10 02:03:54 +0100

    added the new landing page

---
+++ resources/views/includes/dashboard/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="400">
+    <div class="container">
+    <div class="navbar-translate">
+        <a class="navbar-brand" rel="tooltip" title="Designed by Invision. Coded by Creative Tim" data-placement="bottom" target="_blank">
+        <img src="/landing/assets/newassets/img/poic-logo-small.png" alt="Poic logo" style="width: 50%;">
+        </a>
+        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-bar top-bar"></span>
+        <span class="navbar-toggler-bar middle-bar"></span>
+        <span class="navbar-toggler-bar bottom-bar"></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="/landing/assets/newassets/img/blurred-image-1.jpg">
+        <div class="py-2 py-lg-0">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">
+                <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Home</p>
+            </a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">
+                <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">About Us</p>
+            </a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">
+                <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Meet the Winners</p>
+            </a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </div>
+</nav>

796663b2df09ccf10b652a60df23c938dfbda59d
seyi <seyidaniels@gmail.com>
2020-02-21 14:13:37 +0100

    changes

--- resources/views/includes/dashboard/navbar.blade.php
+++ resources/views/includes/dashboard/navbar.blade.php
@@ -1,45 +1,47 @@
 <nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="400">
     <div class="container">
-    <div class="navbar-translate">
-        <a class="navbar-brand" rel="tooltip" title="Designed by Invision. Coded by Creative Tim" data-placement="bottom" target="_blank">
-        <img src="/landing/assets/newassets/img/poic-logo-small.png" alt="Poic logo" style="width: 50%;">
-        </a>
-        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-bar top-bar"></span>
-        <span class="navbar-toggler-bar middle-bar"></span>
-        <span class="navbar-toggler-bar bottom-bar"></span>
-        </button>
-    </div>
-    <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="/landing/assets/newassets/img/blurred-image-1.jpg">
-        <div class="py-2 py-lg-0">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-            <a class="nav-link" href="{{ url('/') }}">
-                <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Home</p>
+        <div class="navbar-translate">
+            <a class="navbar-brand" rel="tooltip" title="" data-placement="bottom" target="_blank">
+                <img src="/landing/assets/newassets/img/poic-logo-small.png" alt="Poic logo" style="width: 50%;">
             </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/rules') }}">
-                    <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Rules</p>
-                </a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="{{ url('/timeline') }}">
-                <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Timelines</p>
-            </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/faq') }}">
-                    <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">FAQ</p>
-                </a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="{{ url('/dashboard') }}">
-                <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Apply Now</p>
-            </a>
-            </li>
-        </ul>
+            <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+                aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-bar top-bar"></span>
+                <span class="navbar-toggler-bar middle-bar"></span>
+                <span class="navbar-toggler-bar bottom-bar"></span>
+            </button>
         </div>
-    </div>
+        <div class="collapse navbar-collapse justify-content-end" id="navigation"
+            data-nav-image="/landing/assets/newassets/img/blurred-image-1.jpg">
+            <div class="py-2 py-lg-0">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/') }}">
+                            <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Home</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/rules') }}">
+                            <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Rules</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/timeline') }}">
+                            <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Timelines</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/faq') }}">
+                            <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">FAQ</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/dashboard') }}">
+                            <p class="text-capitalize underline" style="font-size: 15px;font-weight: 700;">Apply Now</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </nav>

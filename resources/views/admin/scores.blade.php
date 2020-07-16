6267871a9a9899889f465ca39caf3605c172a7fc
seyi <seyidaniels@gmail.com>
2020-07-16 20:36:58 +0100

    Scores

--- resources/views/admin/scores.blade.php
+++ resources/views/admin/scores.blade.php
@@ -12,7 +12,7 @@
           <div class="col-12 col-xl-8">
               <h2>List of Scores</h2>
               <div class="table-responsive" data-toggle="lists" data-lists-values='["tables-row", "tables-first", "tables-last", "tables-handle"]'>
-  <table class="table">
+  <table class="table" id="example">
     <thead>
       <tr>
         <th scope="col">
@@ -54,4 +54,35 @@
     </div>
 
 
+@endsection
+
+
+@section('extra-js')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css
+">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css">
+{{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> --}}
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js
+"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js
+"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js
+"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js
+"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js
+"></script>
+<script>
+  $(document).ready(function() {
+    $('#example').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5'
+        ]
+    } );
+} );
+</script>
 @endsection
